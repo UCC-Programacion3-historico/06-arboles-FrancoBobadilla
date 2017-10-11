b6c19f254711b038c028cf5da908dc55e60a987e
FrancoBobadilla <1709673@ucc.edu.ar>
2017-10-11 11:59:51 -0300

    Terminadas las clases y el ejercicio 1

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "../ArbolBinario/ArbolBinario.h"
-void ProbarArbol(ArbolBinario<int> *arbol, int cont, int min, int max);
+void ProbarArbol(ArbolBinario<int> *arbol, int cont, int min, int max, bool imprimir);
 int main() {
     ArbolBinario<int> arbol;
-    ProbarArbol(&arbol,3000,0,100);
+    try {
+        ProbarArbol(&arbol, 30, 100, 500, true);
+    }catch (int e) {}
     return 0;
 }
 
-void ProbarArbol(ArbolBinario<int> *arbol, int cont, int min, int max) {
+void ProbarArbol(ArbolBinario<int> *arbol, int cont, int min, int max, bool imprimir) {
+    if(cont<0||min>max) throw 1;
     srand(time(NULL));
     while (cont != 0) {
         try {
@@ -15,13 +18,15 @@ void ProbarArbol(ArbolBinario<int> *arbol, int cont, int min, int max) {
             cont--;
         } catch (int e) {}
     }
-    cout << endl << "Arbol de " << cont << " hojas, con valores desde " << min << " hasta " << max << ":" << endl;
-    arbol->print();
-    cout << endl << "Arbol en preorden: " << endl << "   ";
-    arbol->preorder();
-    cout << endl << "Arbol en inorden: " << endl << "   ";
-    arbol->inorder();
-    cout << endl << "Arbol en postorden: " << endl << "   ";
-    arbol->postorder();
-    cout << endl;
+    if(imprimir) {
+        cout << endl << "Arbol de " << cont << " hojas, con valores desde " << min << " hasta " << max << ":" << endl;
+        arbol->print();
+        cout << endl << "Arbol en preorden: " << endl << "   ";
+        arbol->preorder();
+        cout << endl << "Arbol en inorden: " << endl << "   ";
+        arbol->inorder();
+        cout << endl << "Arbol en postorden: " << endl << "   ";
+        arbol->postorder();
+        cout << endl;
+    }
 }
